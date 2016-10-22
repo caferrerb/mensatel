fe33b0b45eaa22d167bb342028ebb4d895070a66
415pc13 <415pc13@415-PC13>
2016-10-22 11:45:05 -0500

    abonados

--- jee/Mensatel/src/java/co/edu/eam/beans/AbonadosBean.java
+++ jee/Mensatel/src/java/co/edu/eam/beans/AbonadosBean.java
@@ -5,6 +5,7 @@
  */
 package co.edu.eam.beans;
 
+import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
@@ -16,11 +17,43 @@ import javax.inject.Named;
 
 
 @ManagedBean
-@Named(value = "abonadoBean")
+@Named(value = "AbonadosBean")
 @ViewScoped
-public class AbonadosBean {
-    
-    
-    
-    
+public class AbonadosBean implements Serializable{
+
+    public String numero;
+    public String documento;
+    public String tipodoc;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getTipodoc() {
+        return tipodoc;
+    }
+
+    public void setTipodoc(String tipodoc) {
+        this.tipodoc = tipodoc;
+    }
+
+    public void mostrar() {
+
+        System.out.println(documento);
+        System.out.println(numero);
+        System.out.println(tipodoc);
+    }
+
 }
